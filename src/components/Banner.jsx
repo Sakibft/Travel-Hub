ff8e629253bde681e8a88ea938750eaa43e305ba
Sakib <ssar96540@gmail.com>
2024-04-28 06:10:08 +0600

    Completed Banner with miningfull informtaion

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,62 +1,67 @@
- 
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import { Autoplay, Navigation, Pagination } from 'swiper/modules';
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
- import '../../src/App.css'
+import "../../src/App.css";
 // import { Pagination, Navigation } from 'swiper/modules';
 const Banner = () => {
   return (
     <div
-    data-aos="zoom-in"
-    data-aos-duration="1000"
-    className="container mx-auto mt-8"
-  >
-    <Swiper
-      slidesPerView={1}
-      spaceBetween={30}
-      loop={true}
-      pagination={{
-        clickable: true,
-      }}
-      autoplay={{ delay: 4000 }}
-      navigation={true}
-      modules={[Navigation, Autoplay,  Pagination]}
-      className="mySwiper"
+      data-aos="zoom-in"
+      data-aos-duration="1000"
+      className="container mx-auto mt-8"
     >
-      <SwiperSlide>
-        <div className="slide slide1">
-          <div>
-            <h1 className="text-white text-center md:text-4xl text-2xl font-bold">
-              Discover information about a potential new  home <br />and subsequently
-              make a purchase.
-              Banckok
-            </h1>
-            <div className="w-full flex justify-center items-center mt-4">
-              <label className="input input-bordered flex rounded-full justify-between items-center gap-2 border-2 hover:border-[#8EA7FF] ">
-                <input type="text" className="grow " placeholder="Search" />
-                <span className="btn rounded-full  bg-[#8EA7FF] text-white hover:bg-[#8EA7FF] ">
-                  Search
-                </span>
-              </label>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Navigation, Autoplay, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <div className="slide slide1">
+            <div>
+              <h1 className="text-white text-center md:text-4xl text-2xl font-bold">
+                Discover information about a potential new country <br />  and Next
+                toured the country
+              </h1>
+              <div className="w-full flex justify-center items-center mt-4">
+                <label className="input input-bordered flex rounded-full justify-between items-center gap-2 border-2 hover:border-[#8EA7FF] ">
+                  <input type="text" className="grow " placeholder="Search" />
+                  <span className="btn rounded-full  bg-pink-400 text-white hover:bg-pink-400 ">
+                    Search
+                  </span>
+                </label>
+              </div>
             </div>
           </div>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="slide slide2"></div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="slide slide3"></div>
-      </SwiperSlide>
-      
-    </Swiper>
-  </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide2">
+          <h1 className="text-white text-center md:text-4xl text-2xl font-bold">
+          Bangladesh is a land of natural beauty, rich culture, <br /> and warm hospitality.
+              </h1>
+
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="slide slide3">
+            <h1 className="text-white text-center md:text-4xl text-2xl font-bold">
+            Bangkok, Thailand’s capital, is a large city known for <br /> ornate shrines and vibrant street life.
+            </h1>
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
   );
 };
 
